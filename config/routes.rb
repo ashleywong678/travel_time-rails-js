7c58bee35101e831fb8e6bd88eb5992aed75c21d
Ashley <ashleywong678@gmail.com>
2019-05-15 11:44:21 -0400

    created routes for signup and agencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,33 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'sessions#welcome'
-  resources :sessions, only: [:new, :create]
+  root to: 'sessions#welcome' #homepage
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+
+  get "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
+
+  #agencies/new => sign up page
+  get "/signup", to: "agencies#new"
+  post "/signup", to: "agencies#create"
+
+  resources :agencies, except: [:new, :create]
 
 end
 
-# Prefix        Verb    URI Pattern                         Controller#Action
-# root          GET     /                                   sessions#welcome
-# sessions      POST    /sessions(.:format)                 sessions#create
-# new_session   GET     /sessions/new(.:format)             sessions#new
+# Prefix    Verb      URI Pattern                      Controller#Action
+# root      GET         /                              sessions#welcome
+# login    GET          /signin(.:format)              sessions#new
+#           POST        /signin(.:format)              sessions#create
+# logout    GET         /logout(.:format)              sessions#destroy
+#           DELETE      /logout(.:format)              sessions#destroy
+# signup    GET         /signup(.:format)              agenciess#new
+#           POST      /signup(.:format)                agenciess#create
+# agencies   GET        /agencies(.:format)            agencies#index
+# edit_agency GET       /agencies/:id/edit(.:format)   agencies#edit
+#agency       GET         /agencies/:id(.:format)      agencies#show
+#             PATCH     /agencies/:id(.:format)        agencies#update
+              # PUT    /agencies/:id(.:format)           agencies#update
+              # DELETE /agencies/:id(.:format)           agencies#destroy
