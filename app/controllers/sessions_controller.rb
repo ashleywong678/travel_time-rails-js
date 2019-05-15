94f2161fc3e1b89df4ff035eb90aef738397b4f8
Ashley <ashleywong678@gmail.com>
2019-05-15 17:36:24 -0400

    redirect to profile if already logged in from login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
 
-  def welcome #root page
+  def welcome #root view page
   end
   
   def new #login view page 
   end
 
   def create #logs user in
+    @agency = Agency.find_by(id: params[:agency][:id])
+    binding.pry
+    if logged_in?
+      redirect_to agency_path(current_user)
+    elsif @agency && @agency.authenticate(params[:password])
+      session[:user_id] = @agency.id
+      redirect_to agency_path(@agency)
+    else
+      render :new
+    end
   end
   
   def destroy
