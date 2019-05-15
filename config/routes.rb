65cc3f4957fb09cada4f6cbda799304ea5c7270b
Ashley <ashleywong678@gmail.com>
2019-05-15 11:10:22 -0400

    created new and create routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'sessions#welcome'
-
+  resources :sessions, only: [:new, :create]
 
 end
+
+# Prefix        Verb    URI Pattern                         Controller#Action
+# root          GET     /                                   sessions#welcome
+# sessions      POST    /sessions(.:format)                 sessions#create
+# new_session   GET     /sessions/new(.:format)             sessions#new
