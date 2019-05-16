5dfd16b2a024798c59929d7a5ab643cbe9977450
Ashley <ashleywong678@gmail.com>
2019-05-16 11:10:46 -0400

    build new action

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -4,9 +4,16 @@ class ToursController < ApplicationController
   end
 
   def new
+    if current_user && logged_in?
+      agency = Agency.find_by(id: current_user.id)
+      @tour = agency.tours.build
+    else
+      redirect_to root_path
+    end
   end
 
   def create
+    binding.pry
   end
 
   def edit
