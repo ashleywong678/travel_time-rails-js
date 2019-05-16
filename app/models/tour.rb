becd6f1f732b8dfb920a4bbd0c31197bab279d96
Ashley <ashleywong678@gmail.com>
2019-05-16 10:01:04 -0400

    generated tours model and migration table

---
+++ app/models/tour.rb
@@ -0,0 +1,8 @@
+class Tour < ApplicationRecord
+  belongs_to :agency
+
+  def self.most_popular #returns list of ordered tours by most customer signup
+
+  end
+
+end
---
+++ db/migrate/20190516135314_create_tours.rb
@@ -0,0 +1,10 @@
+class CreateTours < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tours do |t|
+      t.string :country
+      t.date :date
+      t.integer :price
+      t.integer :agency_id
+    end
+  end
+end
