ee2e0691985427a6c4b91c84d56707ac447fefa7
Ashley <ashleywong678@gmail.com>
2019-05-17 12:08:26 -0400

    added scope method to return all tours for an agency

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,5 +1,7 @@
 class Tour < ApplicationRecord
   belongs_to :agency
+  scope :my_tours, -> (agency) { where("agency_id = ?", agency.id) }
+
 
   def self.most_popular #returns list of ordered tours by most customer signup
 
