410b8af95c8af0e374bfd1f78eefabeee8ac61b9
Ashley <ashleywong678@gmail.com>
2019-05-18 10:50:20 -0400

    added note for potential change in omniauth initializer

--- config/initializers/onmiauth.rb
+++ config/initializers/onmiauth.rb
@@ -1,3 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
+#   scope: ['email',
+#     'https://www.googleapis.com/auth/gmail.modify'],
+#     access_type: 'offline'}
+# end
