ea9fcfb15e17abd55b4fbdcd572040767d5f773f
Ashley <ashleywong678@gmail.com>
2019-05-19 11:50:23 -0400

    added method to print out customer full name and formal full name

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,13 @@ class Customer < ApplicationRecord
   has_many :customer_tours
   has_many :tours, through: :customer_tours
   has_many :agencies, through: :tours
+
+  def fullname
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def formalname
+    "#{self.last_name}, #{self.first_name}"
+  end
   
 end
