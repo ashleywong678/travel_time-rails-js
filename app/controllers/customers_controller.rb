844f1e1ed79e519bb45f4ddace0318b3ca26f4dc
Ashley <ashleywong678@gmail.com>
2019-05-19 10:29:54 -0400

    added logic to index to account for nested and unnested routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,8 +1,15 @@
 class CustomersController < ApplicationController
   def index
+    binding.pry
+    if params[:tour_id] && logged_in?
+      @customers = Customer.find_by(tour_id: params[:tour_id])
+    else
+      @customers = Customer.all
+    end
   end
 
   def new
+    @customer = Customer.new
   end
 
   def create
@@ -19,4 +26,5 @@ class CustomersController < ApplicationController
 
   def destroy
   end
+
 end
