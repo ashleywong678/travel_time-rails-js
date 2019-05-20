686f8c3911ccf894cd62e9f45109277360d5e620
Ashley <ashleywong678@gmail.com>
2019-05-19 20:51:19 -0400

    built create method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,12 +10,21 @@ class CustomersController < ApplicationController
 
   def new
     @customer = Customer.new
+    @tour = Tour.find_by(id: params[:tour_id]) if params[:tour_id]
   end
 
   def create
+    @customer = Customer.new(customer_params)
+    @customer.tours << Tour.find_by(id: params[:tour_id]) if !@customer.tours.include?(params[:tour_id]) if params[:tour_id]
+    if @customer.save
+      redirect_to @customer
+    else
+      render :new
+    end
   end
 
   def show
+    @customer = Customer.find_by(id: params[:id])
   end
 
   def edit
@@ -25,6 +34,12 @@ class CustomersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone_number, :city, :country, :language, :tour_id)
   end
 
 end
