8c53df8cc3cd00472ada2b67ab73b9316c14e50d
Ashley <ashleywong678@gmail.com>
2019-05-21 19:43:05 -0400

    added validations and custom validation

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -3,10 +3,31 @@ class Tour < ApplicationRecord
   has_many :customer_tours
   has_many :customers, through: :customer_tours
   validates :title, :country, :date, :length, :price, :agency_id, presence: true
+  validates :length, :price, numericality: true
+  validates :length, length: { minimum: 1 }
+  validates :length, :price, numericality: { only_integer: true }
+  validates :price, length: { minimum: 1 }
+  validates :price, numericality: { greater_than: 0 }
+  validates :todays_date, confirmation: true
 
+  def todays_date #check if :date is after current date
+    if date.present?
+      errors[:date] << "date must be in the future"
+    end
+  end
 
   def self.most_popular #returns list of ordered tours by most customer signup
 
   end
 
 end
+
+
+# validate :expiration_date_cannot_be_in_the_past,
+#     :discount_cannot_be_greater_than_total_value
+ 
+#   def expiration_date_cannot_be_in_the_past
+#     if expiration_date.present? && expiration_date < Date.today
+#       errors.add(:expiration_date, "can't be in the past")
+#     end
+  # end
