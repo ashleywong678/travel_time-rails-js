08edb209330a0f8fe093675de3537507922eadb2
Ashley <ashleywong678@gmail.com>
2019-05-21 19:44:46 -0400

    made route dynamic for nested and unnested

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+
   def index
     if params[:tour_id] && logged_in?
       @customers = Tour.find_by(id: params[:tour_id]).customers
@@ -8,15 +9,28 @@ class CustomersController < ApplicationController
   end
 
   def new
-    @customer = Customer.new
-    @tour = Tour.find_by(id: params[:tour_id]) if params[:tour_id]
+    if params[:tour_id]
+      @tour = Tour.find_by(id: params[:tour_id]) if params[:tour_id]
+      @customer = @tour.customers.build
+    else
+      @customer = Customer.new
+    end
   end
 
   def create
-    @customer = Customer.new(customer_params)
-    @customer.tours << Tour.find_by(id: params[:tour_id]) if !@customer.tours.include?(params[:tour_id]) if params[:tour_id]
-    if @customer.save
-      redirect_to @customer
+    if params[:tour_id]
+    @tour = Tour.find_by(id: params[:tour_id])
+    @customer = Customer.create(customer_params)
+    if @customer
+        ct = @tour.customer_tours.build(customer_id: @customer.id, notes: params[:notes])
+        ct.save
+        redirect_to @customer
+      end
+    elsif
+      @customer = Customer.create(customer_params)
+      if @customer
+        redirect_to @customer
+      end
     else
       render :new
     end
@@ -24,24 +38,47 @@ class CustomersController < ApplicationController
 
   def show
     @customer = Customer.find_by(id: params[:id])
+    @notes = @customer.customer_tours
   end
 
   def edit
     @customer = Customer.find_by(id: params[:id])
-    @tours = @customer.tours
-    @all_tours = Tour.all
+    @tours  = Tour.all
   end
 
   def update
+    @customer = Customer.find_by(id: params[:id])
+    @customer.update(customer_params)
+    if tours = params[:customer][:tour_ids] 
+      tours.each do |tour|
+        t = Tour.find_by(id: tour.to_i)
+        if t
+          # binding.pry
+          if !@customer.tours.include?(t)
+            ct = @customer.customer_tours.build(notes: params[:notes], tour_id: t.id) 
+            #notes not being added, need to fix
+            ct.save
+          end
+        end
+      end
+    end
+    redirect_to @customer
   end
 
   def destroy
+    @customer = Customer.find_by(id: params[:id])
+    if @customer && logged_in?
+      @customer.destroy
+      redirect_to customers_path
+    else
+      redirect_to @customer
+    end
   end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :email, :phone_number, :city, :country, :language, :tour_id)
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone_number, :city, :country, :language, :tour_ids, :customer_tours)
   end
 
 end
