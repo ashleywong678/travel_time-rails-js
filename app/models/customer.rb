bb13023cd346c078fa268216554b420fb6b7eca3
Ashley <ashleywong678@gmail.com>
2019-05-22 08:43:51 -0400

    removed validation for phone number, interferring with google auth

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,16 +2,13 @@ class Customer < ApplicationRecord
   has_many :customer_tours
   has_many :tours, through: :customer_tours
   has_many :agencies, through: :tours
+  
   validates :first_name, :last_name, :email, presence: true
   validates :email, uniqueness: true
   validates :phone_number, length: { is: 10 }
 
   def full_name
     "#{self.first_name} #{self.last_name}"
-  end
-
-  def formal_name
-    "#{self.last_name}, #{self.first_name}"
   end
   
 end
