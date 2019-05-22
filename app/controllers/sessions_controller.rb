e14af7cbdcf9e25934bd9da99398dbcdd5e4b8f6
Ashley <ashleywong678@gmail.com>
2019-05-21 20:27:48 -0400

    added flash message for invalid login and styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,18 @@ class SessionsController < ApplicationController
   def welcome #root view page
   end
   
-  def new #login view page 
+  def new #login view page
   end
 
   def create #logs user in
-      @agency = Agency.find_by(id: params[:agency][:id])
+    @agency = Agency.find_by(id: params[:agency][:id])
       if logged_in?
         redirect_to agencies_main_path(@agency)
       elsif @agency && @agency.authenticate(params[:password])
         session[:user_id] = @agency.id
         redirect_to agencies_main_path(@agency)
       else
+        flash[:message] = "Invalid login. Please try again."
         render :new
       end
   end
