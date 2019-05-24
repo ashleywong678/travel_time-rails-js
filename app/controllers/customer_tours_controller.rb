f4453ffa5890cd2f5e55a310f45799034381e946
Ashley <ashleywong678@gmail.com>
2019-05-24 12:34:05 -0400

    fixed bugs in create and update

--- app/controllers/customer_tours_controller.rb
+++ app/controllers/customer_tours_controller.rb
@@ -1,7 +1,6 @@
 class CustomerToursController < ApplicationController
 
   def update
-    binding.pry
     customer_tour = CustomerTour.find_by(id: params[:id])
     customer_tour.canceled = true
     customer_tour.save
