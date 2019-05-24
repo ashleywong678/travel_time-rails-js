70f1924561c03e5631c1945d95843fe537869a8b
Ashley <ashleywong678@gmail.com>
2019-05-24 12:33:23 -0400

    removed notes and extra method

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -21,13 +21,3 @@ class Tour < ApplicationRecord
   end
 
 end
-
-
-# validate :expiration_date_cannot_be_in_the_past,
-#     :discount_cannot_be_greater_than_total_value
- 
-#   def expiration_date_cannot_be_in_the_past
-#     if expiration_date.present? && expiration_date < Date.today
-#       errors.add(:expiration_date, "can't be in the past")
-#     end
-  # end
