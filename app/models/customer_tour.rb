beea2b4f37dda5ae9955f2d4d7b0289bd8331878
Ashley <ashleywong678@gmail.com>
2019-05-24 12:36:02 -0400

    cleaned up model

--- app/models/customer_tour.rb
+++ app/models/customer_tour.rb
@@ -1,7 +1,6 @@
 class CustomerTour < ApplicationRecord
   belongs_to :tour
   belongs_to :customer
-  # validates :tour_id, :customer_id, presenced: true
   
   def self.find_join(customer, tour)
     self.find_by(customer_id: customer.id, tour_id: tour.id)
