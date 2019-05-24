d9155f8f69a49dcae13128debb6cedee50114cd6
Ashley <ashleywong678@gmail.com>
2019-05-24 15:01:44 -0400

    added helper require login for all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :require_login
 
   def current_user
     @current_user ||= Agency.find_by(id: session[:user_id]) if session[:user_id]
@@ -8,6 +8,13 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:message] = "Please login or signup."
+      redirect_to root_path
+    end
   end
 
 end
