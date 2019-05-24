68533e93ba1efca59bccf30e2b48d0731b12a4f7
Ashley <ashleywong678@gmail.com>
2019-05-24 19:22:51 -0400

    created scope method for only active customers

--- app/models/customer_tour.rb
+++ app/models/customer_tour.rb
@@ -1,6 +1,7 @@
 class CustomerTour < ApplicationRecord
   belongs_to :tour
   belongs_to :customer
+  scope :active, -> { where(canceled: 'false') }
   
   def self.find_join(customer, tour)
     self.find_by(customer_id: customer.id, tour_id: tour.id)
