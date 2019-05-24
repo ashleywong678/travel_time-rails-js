d570f38fe1cd07fe482e2764bb826242563a0833
Ashley <ashleywong678@gmail.com>
2019-05-24 15:04:45 -0400

    fixed bugs on validation of date

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,17 +2,19 @@ class Tour < ApplicationRecord
   belongs_to :agency
   has_many :customer_tours
   has_many :customers, through: :customer_tours
-  validates :title, :country, :date, :length, :price, :agency_id, presence: true
+  validates :todays_date, confirmation: true
+  validates :title, :country, :length, :price, :agency_id, presence: true
   validates :length, :price, numericality: true
   validates :length, length: { minimum: 1 }
   validates :length, :price, numericality: { only_integer: true }
   validates :price, length: { minimum: 1 }
   validates :price, numericality: { greater_than: 0 }
-  validates :todays_date, confirmation: true
 
   def todays_date #check if :date is after current date
-    if date.past? || date.today?
-      errors[:date] << "date must be in the future"
+    if date == nil
+      errors[:date] << "cannot be blank"
+    elsif date.past? || date.today?
+      errors[:date] << "must be after today"
     end
   end
 
