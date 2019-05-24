78d61f71ad0e11692174154f0f5989bd4781fa9b
Ashley <ashleywong678@gmail.com>
2019-05-24 12:36:44 -0400

    fixed bugs in create and update

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,5 @@
 class CustomersController < ApplicationController
+  before_action :require_login
 
   def index
     if params[:tour_id] && logged_in?
@@ -24,17 +25,17 @@ class CustomersController < ApplicationController
     if @customer
       flash[:message] = "Customer already exists"
       redirect_to customers_path        #send to customer index
-
     elsif !@customer    #if customer doesn't already exist
+      # binding.pry
       @customer = Customer.create(customer_params)   #create customer
-      if params[:customer][:tour_ids]  #if the form is nested
+      if params[:customer][:tour_id]  #if the form is nested
         @tour = Tour.find_by(id: params[:customer][:tour_id])
         customer_tour = @tour.customer_tours.build(customer_id: @customer.id)    #creat a join table between @tour and @customer
-        customer_tour.save    #save b/c build doesn't persist
+        customer_tour.save
       end
+      @customer.save
       redirect_to @customer   #then go to customer show page
-
-    else    #if everythign fails...
+    else    #if everything fails...
       render :new
     end
   end
@@ -53,23 +54,8 @@ class CustomersController < ApplicationController
     @customer.update(customer_params)
     if @customer.errors.any?
       render :edit
-    elsif tours = params[:customer][:tour_ids]
-      tours.delete("")
-      tours.each do |tour|    #tour is a integer for Tour.id
-        customer_tour = CustomerTour.find_join(@customer, Tour.find_by(id: tour))    #look for a join with both objects
-        if !customer_tour     #if there's no join between customer and tour
-          customer_tour = found_tour.customer_tours.build(customer_id: @customer.id)    #creat a join table between tour and @customer
-          customer_tour.save    
-        #elsif...a tour has been removed
-        # change join.canceled => true
-        end
-      end 
-      redirect_to @customer
     end
-    #   - find customer
-    #   -update customer by params
-    #   -add tours to customer.tours if they're new
-    #   -if tour is removed from customer, change customer_tours.canceled => false
+    redirect_to @customer
   end
 
   def destroy
@@ -85,7 +71,14 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :email, :phone_number, :city, :country, :language, :tour_ids, :customer_tours)
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone_number, :city, :country, :language, :customer_tours, tour_ids:[])
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:message] = "Please login or signup."
+      redirect_to root_path
+    end
   end
 
 end
