82cb36ce42fd59aaaa2f41380f0bdeece1bb169c
Ashley <ashleywong678@gmail.com>
2019-07-05 22:38:06 -0400

    first commit

---
+++ app/serializers/tour_serializer.rb
@@ -0,0 +1,7 @@
+class TourSerializer < ActiveModel::Serializer
+  attributes :id, :title, :country, :date, :length, :price, :description
+
+  belongs_to :agency
+  has_many :customer_tours
+  has_many :customers, through :customer_tours
+end
