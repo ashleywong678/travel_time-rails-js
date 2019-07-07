c46bd7241257b26b4ba914269d4d143d48ea96bd
Ashley <ashleywong678@gmail.com>
2019-07-07 15:52:44 -0400

    edited client_id label

--- config/initializers/onmiauth.rb
+++ config/initializers/onmiauth.rb
@@ -1,10 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
-end
-
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-#   scope: ['email',
-#     'https://www.googleapis.com/auth/gmail.modify'],
-#     access_type: 'offline'}
-# end
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+end 
