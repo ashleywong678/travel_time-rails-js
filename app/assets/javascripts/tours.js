e1ddfade8b34ead67e76dc51a636879348dd616b
Ashley <ashleywong678@gmail.com>
2019-07-09 20:31:34 -0400

    added fetch logic

--- app/assets/javascripts/tours.js
+++ app/assets/javascripts/tours.js
@@ -3,7 +3,36 @@ $(() => {
 })
 
 const myTours = () => {
-  $('.my_tours').on('click', (event) => {
-    event.preventDefault()
+  $('.my_tours').on('click', (e) => {
+    e.preventDefault()
+    history.pushState("", "", "tours")
+    fetch('/tours.json')
+      .then(res => res.json())
+      .then(tours => {
+        $('.main').html('')
+        tours.forEach(tour => {
+          let newTour = new Tour(tour)
+          let tourHtml = newTour.formatIndex()
+          $('.main').append(tourHtml)
+        })
+      })
   })
+}
+
+function Tour(tour){
+  this.id = tour.id
+  this.id = tour.title
+  this.date = tour.date
+  this.length = tour.length
+  this.price =  tour.price
+  this.country = tour.country
+  this.description = tour.description
+}
+
+Tour.prototype.formatIndex = function(){
+  let tourHtml = `
+  <li><a href='/tour/${this.id}'>${this.title}</a>
+  </li>
+  `
+  return tourHtml
 }
