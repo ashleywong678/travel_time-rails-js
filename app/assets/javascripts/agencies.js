d4944a922dcb5d5e2bc06c17e5bcdb3d2704ea55
Ashley <ashleywong678@gmail.com>
2019-07-10 22:32:46 -0400

    began logic for fetch request for show route

--- app/assets/javascripts/agencies.js
+++ app/assets/javascripts/agencies.js
@@ -3,13 +3,30 @@ $(() => {
 })
 
 const profile = () => {
-  $('.my_profile').on('click', (event) => {
-    event.preventDefault()
-    fetch(`/agencies/${current_user.id}.josn`)
-      .then((resp) => resp.json())
-      .then(data => {
-        $('#info').html('')
-        data
+  $(document).on('click', '.my_profile', function(e){
+    e.preventDefault()
+    let url = this.document.URL
+    fetch(`${url}.json`)
+      .then(res => res.json())
+      .then(agency => {
+        console.log(agency)
       })
   })
+}
+
+function Agency(agency){
+  this.id = agency.id
+  this.name = agency.name
+  this.country = agency.country
+  this.email = agency.email
+  this.address = agency.address
+  this.language = agency.language
+  this.phone_number = agency.phone_number
+  this.tours = agency.tours
+}
+
+Agency.prototype.formatIndex = function(){
+  let agencyHtml = `
+  `
+  return agencyHtml
 }
