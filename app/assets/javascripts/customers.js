4b75d859f815ab31cbb3b270f45abf1c39e970f4
Ashley <ashleywong678@gmail.com>
2019-07-13 15:04:48 -0400

    added customer info and buttons fo redit and delete when js is rendered

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -1,13 +1,13 @@
 $(() => {
-  allCustomers()
+  customersListeners()
 })
 
-const allCustomers = () => {
+const customersListeners = () => {
   //show all customers from agency main page
   $('.all_customers').on('click', (e) => {
     e.preventDefault()
     const data = 'http://localhost:3000/'
-    url = data + "customers"
+    let url = data + "customers"
     history.pushState(null, null, url)
     getCustomers()
   })
@@ -15,6 +15,8 @@ const allCustomers = () => {
   $(document).on('click', ".show_customer", function(e){
     e.preventDefault()
     let id = $(this).attr('data-id')
+    let c_url = 'http://localhost:3000/customers/' + id
+    history.pushState(null, null, c_url)
     fetch(`/customers/${id}.json`)
     .then(res => res.json())
     .then(customer => {
@@ -24,6 +26,8 @@ const allCustomers = () => {
         $(".main").append(customerHtml)
     })
   })
+  //new customer form submission
+  $()
 }
 
 function Customer(customer){
@@ -49,7 +53,16 @@ Customer.prototype.formatIndex = function(){
 
 Customer.prototype.formatShow = function(){
   let customerHtml = `
-  <h1 class="center">${this.first_name} ${this.last_name}</h1>
+    <h1 class="center">${this.first_name} ${this.last_name}</h1>
+    <p><strong>Email: ${this.email}</strong></p>
+    <p><strong>Phone number: ${this.phone_number}</strong></p>
+    <p><strong>City: ${this.city}</strong></p>
+    <p><strong>Country: ${this.country}</strong></p>
+    <p><strong>Prefered Language: ${this.language}</strong></p>
+    <p><strong>Signed up tours: </strong></p><ul></ul>
+    <br>
+    <a href='${this.id}/edit' class='btn'>Edit this Customer</a>
+    <a class="btn" rel="nofollow" data-method="delete" href="/customers/${this.id}">Delete Customer</a>
   `
   return customerHtml
 }
@@ -59,12 +72,12 @@ const getCustomers = () => {
       .then(res => res.json())
       .then(customers => {
         $('.main').html('')
-        $('.main').append('<h1 class="center">Customer Accounts</h1><br><ul class="left" id="info"></ul>')
+        $('.main').append('<h1 class="center">Customer Accounts</h1><br><ul id="info"></ul>')
         customers.forEach(customer => {
           let newCustomer = new Customer(customer)
           let customerHtml = newCustomer.formatIndex()
           $("#info").append(customerHtml)
         })
-        $('.main').append('<br><br><a href="/customers/new" class="btn left">Create a Customer</a><br><br>')
+        $('.main').append('<br><br><a href="/customers/new" class="btn">Create a Customer</a>')
       })
 }
