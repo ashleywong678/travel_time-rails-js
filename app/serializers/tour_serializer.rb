2c574fdbb785d463db865adbca607f6fd48d1dd4
Ashley <ashleywong678@gmail.com>
2019-07-13 23:38:20 -0400

    added connected serializer to customer_tours

--- app/serializers/tour_serializer.rb
+++ app/serializers/tour_serializer.rb
@@ -1,7 +1,7 @@
 class TourSerializer < ActiveModel::Serializer
-  attributes :id, :title, :country, :date, :length, :price, :description
+  attributes :id, :title, :country, :date, :length, :price, :description, :agency_id
 
   belongs_to :agency
-  has_many :customer_tours
+  has_many :customer_tours, serializer: CustomerTourSerializer
   
 end
