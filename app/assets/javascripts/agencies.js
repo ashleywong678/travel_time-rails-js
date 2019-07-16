0471ff8544413d1c1f4ee57f113239d45661f613
Ashley <ashleywong678@gmail.com>
2019-07-15 21:44:36 -0400

    built fetch request for profile and edited bugs

--- app/assets/javascripts/agencies.js
+++ app/assets/javascripts/agencies.js
@@ -5,11 +5,19 @@ $(() => {
 const profile = () => {
   $(document).on('click', '.my_profile', function(e){
     e.preventDefault()
-    let url = this.document.URL
-    fetch(`${url}.json`)
+    let id = window.location.pathname.split('/')[2]
+    let url = 'http://localhost:3000/agencies/' + id
+    history.pushState(null, null, url)
+    console.log(id)
+    fetch(`/agencies/${id}.json`)
       .then(res => res.json())
       .then(agency => {
+        $('.main').html('')
         console.log(agency)
+        let newAgency = new Agency(agency)
+        let agencyHtml = newAgency.formatIndex()
+        $(".main").append(agencyHtml)
+        $(".main").append(`<br><a class="btn" href="/agencies/${id}/edit">Edit Profile</a>`)
       })
   })
 }
@@ -27,6 +35,14 @@ function Agency(agency){
 
 Agency.prototype.formatIndex = function(){
   let agencyHtml = `
+  <h1 class="center">${this.name}</h1>
+  <ul>
+      <p><strong>Email: </strong> ${this.email}</p>
+      <p><strong>Address:</strong> ${this.address}</p>
+      <p><strong>Phone Number:</strong> ${this.phone_number}</p>
+      <p><strong>Country:</strong> ${this.country}</p>
+      <p><strong>Main Language:</strong> ${this.language}</p>
+    </ul>
   `
   return agencyHtml
 }
